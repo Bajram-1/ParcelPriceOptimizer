fa2898371870dab4af6fc4f8c764d928e18cfa3c
Bajram Shehi <bajram.shehi98@gmail.com>
2024-11-08 11:18:04 +0100

    configured sql connection string to be used in project by DALStartup

---
+++ ParcelPriceOptimizer.DAL/DALStartup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParcelPriceOptimizer.DAL
+{
+    public static class DALStartup
+    {
+        public static void RegisterDALServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+        }
+    }
+}
