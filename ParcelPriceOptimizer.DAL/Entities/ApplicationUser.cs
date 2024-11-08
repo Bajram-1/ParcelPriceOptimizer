be882a290b9c84241e5bdc82233f4d0b964b30f2
Bajram Shehi <bajram.shehi98@gmail.com>
2024-11-08 12:00:07 +0100

    User Entity implementation

---
+++ ParcelPriceOptimizer.DAL/Entities/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ParcelPriceOptimizer.DAL.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string Role { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public ICollection<CustomerInput> CustomerInputs { get; set; }
+    }
+}
