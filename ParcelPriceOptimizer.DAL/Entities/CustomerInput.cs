7f936e51285d60314a48ea45c88220f75faaf856
Bajram Shehi <bajram.shehi98@gmail.com>
2024-11-08 12:04:18 +0100

    CustomerInput entity implementation

---
+++ ParcelPriceOptimizer.DAL/Entities/CustomerInput.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParcelPriceOptimizer.DAL.Entities
+{
+    public class CustomerInput : BaseEntityWithKey
+    {
+        public int ParcelId { get; set; }
+        public Parcel Parcel { get; set; }
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerEmail { get; set; }
+        public DateTime SubmittedAt { get; set; } = DateTime.UtcNow;
+
+    }
+}
