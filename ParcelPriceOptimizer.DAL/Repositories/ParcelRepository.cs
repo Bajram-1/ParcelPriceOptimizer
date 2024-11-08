2d54fea4fb3ff3349486f21e47e9541aa13495ea
Bajram Shehi <bajram.shehi98@gmail.com>
2024-11-08 15:37:13 +0100

    Parcel Repository implementation

---
+++ ParcelPriceOptimizer.DAL/Repositories/ParcelRepository.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParcelPriceOptimizer.DAL.Entities;
+using ParcelPriceOptimizer.DAL.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParcelPriceOptimizer.DAL.Repositories
+{
+    public class ParcelRepository : IParcelRepository
+    {
+        private readonly List<Parcel> _parcels = new();
+        private readonly ApplicationDbContext _context;
+
+        public ParcelRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Parcel>> GetAllAsync()
+        {
+            return await _context.Parcels.Include(p => p.CustomerInput)
+                                         .Include(p => p.ShippingQuotes)
+                                         .ToListAsync();
+        }
+        public async Task<Parcel> GetByIdAsync(int id)
+        {
+            return await _context.Parcels.Include(p => p.CustomerInput)
+                                         .Include(p => p.ShippingQuotes)
+                                         .FirstOrDefaultAsync(p => p.Id == id);
+        }
+        public async Task AddAsync(Parcel parcel)
+        {
+            parcel.Id = _parcels.Count + 1;
+            _parcels.Add(parcel);
+            await Task.CompletedTask;
+        }
+        public async Task UpdateAsync(Parcel parcel)
+        {
+            var existingParcel = _parcels.FirstOrDefault(p => p.Id == parcel.Id);
+            if (existingParcel != null)
+            {
+                existingParcel.Width = parcel.Width;
+                existingParcel.Height = parcel.Height;
+                existingParcel.Depth = parcel.Depth;
+                existingParcel.Weight = parcel.Weight;
+                existingParcel.Price = parcel.Price;
+            }
+            await Task.CompletedTask;
+        }
+        public async Task DeleteAsync(int id)
+        {
+            var parcel = _parcels.FirstOrDefault(p => p.Id == id);
+            if (parcel != null)
+            {
+                _parcels.Remove(parcel);
+            }
+            await Task.CompletedTask;
+        }
+    }
+}
