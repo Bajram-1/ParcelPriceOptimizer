3633ae71833afe5945f24da9d4223eda8004a317
Bajram Shehi <bajram.shehi98@gmail.com>
2024-11-08 15:35:37 +0100

    Courier Pricing Rule Repository Implementation

---
+++ ParcelPriceOptimizer.DAL/Repositories/CourierPricingRuleRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParcelPriceOptimizer.DAL.Entities;
+using ParcelPriceOptimizer.DAL.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParcelPriceOptimizer.DAL.Repositories
+{
+    public class CourierPricingRuleRepository : ICourierPricingRuleRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CourierPricingRuleRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<CourierPricingRule>> GetAllAsync()
+        {
+            return await _context.CourierPricingRules.ToListAsync();
+        }
+
+        public async Task AddAsync(CourierPricingRule rule)
+        {
+            await _context.AddAsync(rule);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<CourierPricingRule> GetByIdAsync(int id)
+        {
+            return await _context.CourierPricingRules.FindAsync(id);
+        }
+    }
+}
