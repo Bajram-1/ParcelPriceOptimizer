e49bd74aba16a9d02484fc0d0a9fb62b78d8c46a
Bajram Shehi <bajram.shehi98@gmail.com>
2024-11-09 10:26:11 +0100

    Customer Input Service implementation

---
+++ ParcelPriceOptimizer.BLL/IServices/ICustomerInputService.cs
@@ -0,0 +1,10 @@
+﻿using ParcelPriceOptimizer.BLL.DTO.ViewModels;
+using ParcelPriceOptimizer.DAL.Entities;
+
+namespace ParcelPriceOptimizer.BLL.IServices
+{
+    public interface ICustomerInputService
+    {
+        Task SaveCustomerInputAsync(PackageInputViewModel input, decimal price);
+    }
+}
