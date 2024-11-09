6f38791ecccd95d50169ec254c376c233b190833
Bajram Shehi <bajram.shehi98@gmail.com>
2024-11-09 10:28:22 +0100

    Shipping Cost Service implementation

---
+++ ParcelPriceOptimizer.BLL/Services/ShippingCostService.cs
@@ -0,0 +1,35 @@
+﻿using ParcelPriceOptimizer.BLL.DTO.ViewModels;
+using ParcelPriceOptimizer.BLL.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParcelPriceOptimizer.BLL.Services
+{
+    public class ShippingCostService : IShippingCostService
+    {
+        private readonly IPriceCalculationService _priceCalculationService;
+
+        public ShippingCostService(IPriceCalculationService priceCalculationService)
+        {
+            _priceCalculationService = priceCalculationService;
+        }
+
+        public async Task<decimal> GetShippingCostAsync(PackageInputViewModel input)
+        {
+            return await Task.Run(() => _priceCalculationService.CalculatePrice(input));
+        }
+
+        public async Task<IEnumerable<decimal>> GetAllShippingCostsAsync(IEnumerable<PackageInputViewModel> inputs)
+        {
+            var costs = new List<decimal>();
+            foreach (var input in inputs)
+            {
+                costs.Add(await GetShippingCostAsync(input));
+            }
+            return costs;
+        }
+    }
+}
