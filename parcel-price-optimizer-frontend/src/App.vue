5f0ed2843c4aaa56a0176e3c853a4435f5db7103
Bajram Shehi <bajram.shehi98@gmail.com>
2024-11-09 11:18:57 +0100

    Initial commit: Frontend Structure

---
+++ parcel-price-optimizer-frontend/src/App.vue
@@ -0,0 +1,13 @@
+<template> 
+  <div id="app"> 
+    <h1>Custom Title: Welcome to Parcel Price Optimizer</h1> 
+    <router-view/> 
+  </div> 
+</template> 
+
+<script> 
+  export default { name: 'App' } 
+</script> 
+
+<style>
+</style>
