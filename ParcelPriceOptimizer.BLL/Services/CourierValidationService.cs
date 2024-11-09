fb0480343b9e4e36d10d4fceec1b6aabd48d9694
Bajram Shehi <bajram.shehi98@gmail.com>
2024-11-09 10:25:23 +0100

    Courier Validation Service implementation

---
+++ ParcelPriceOptimizer.BLL/Services/CourierValidationService.cs
@@ -0,0 +1,31 @@
+﻿using ParcelPriceOptimizer.BLL.DTO.ViewModels;
+using ParcelPriceOptimizer.BLL.IServices;
+using ParcelPriceOptimizer.DAL.IRepositories;
+
+namespace ParcelPriceOptimizer.BLL.Services
+{
+    public class CourierValidationService : ICourierValidationService
+    {
+        private ICourierRepository _courierRepository;
+
+        public CourierValidationService(ICourierRepository courierRepository)
+        {
+            _courierRepository = courierRepository;
+        }
+
+        public bool IsValidForCompany1(PackageInputViewModel input)
+        {
+            return input.Weight <= 20 && input.Weight > 0 && input.Volume <= 2000;
+        }
+
+        public bool IsValidForCompany2(PackageInputViewModel input)
+        {
+            return input.Weight > 10 && input.Weight <= 30 && input.Volume <= 1700;
+        }
+
+        public bool IsValidForCompany3(PackageInputViewModel input)
+        {
+            return input.Weight >= 10 && input.Volume >= 500;
+        }
+    }
+}
