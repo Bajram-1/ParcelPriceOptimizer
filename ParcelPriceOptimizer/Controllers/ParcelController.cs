52b0d3b74700a90b6dc583e3b331c2b29714dda1
Bajram Shehi <bajram.shehi98@gmail.com>
2024-11-09 10:36:30 +0100

    Parcel API Controller implementation

---
+++ ParcelPriceOptimizer/Controllers/ParcelController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ParcelPriceOptimizer.BLL.DTO.ViewModels;
+using ParcelPriceOptimizer.BLL.IServices;
+
+namespace ParcelPriceOptimizer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ParcelController : ControllerBase
+    {
+        private readonly IPriceCalculationService _priceCalculationService;
+        private readonly ICustomerInputService _customerInputService;
+
+        public ParcelController(IPriceCalculationService priceCalculationService, ICustomerInputService customerInputService)
+        {
+            _priceCalculationService = priceCalculationService;
+            _customerInputService = customerInputService;
+        }
+
+        [HttpPost("calculate")]
+        public async Task<IActionResult> CalculatePrice([FromBody] PackageInputViewModel input)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid input data.");
+            }
+
+            decimal price = _priceCalculationService.CalculatePrice(input);
+
+            return Ok(new { price });
+        }
+    }
+}
