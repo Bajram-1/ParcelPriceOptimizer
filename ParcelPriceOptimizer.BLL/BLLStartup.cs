7bf687e3957b2b6e029033aaf042fdf87b160518
Bajram Shehi <bajram.shehi98@gmail.com>
2024-11-09 10:29:22 +0100

    Declared and Registered services scope in BLL Startup

---
+++ ParcelPriceOptimizer.BLL/BLLStartup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ParcelPriceOptimizer.BLL.IServices;
+using ParcelPriceOptimizer.BLL.Services;
+
+namespace ParcelPriceOptimizer.BLL
+{
+    public static class BLLStartup
+    {
+        public static void RegisterBLLServices(this IServiceCollection services)
+        {
+            services.AddScoped<ICourierPricingService, CourierPricingService>();
+            services.AddScoped<ICourierValidationService, CourierValidationService>();
+            services.AddScoped<ICustomerInputService, CustomerInputService>();
+            services.AddScoped<IPriceCalculationService, PriceCalculationService>();
+            services.AddScoped<IShippingCostService, ShippingCostService>();
+        }
+    }
+}
