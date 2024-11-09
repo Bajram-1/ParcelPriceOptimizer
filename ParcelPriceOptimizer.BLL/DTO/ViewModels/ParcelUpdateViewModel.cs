09dc15e2dc4fca6860ee2f68ed4fa88387eb91d8
Bajram Shehi <bajram.shehi98@gmail.com>
2024-11-09 10:30:14 +0100

    Parcel view, create and update implementation

---
+++ ParcelPriceOptimizer.BLL/DTO/ViewModels/ParcelUpdateViewModel.cs
@@ -0,0 +1,24 @@
+﻿using ParcelPriceOptimizer.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParcelPriceOptimizer.BLL.DTO.ViewModels
+{
+    public class ParcelUpdateViewModel
+    {
+        public int Id { get; set; }
+        public decimal Width { get; set; }
+        public decimal Height { get; set; }
+        public decimal Depth { get; set; }
+        public decimal Weight { get; set; }
+        public decimal Volume => Width * Height * Depth;
+        public decimal Price { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public int CustomerInputId { get; set; }
+        public CustomerInput CustomerInput { get; set; }
+        public ICollection<ShippingQuote> ShippingQuotes { get; set; }
+    }
+}
