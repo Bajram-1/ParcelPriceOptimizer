d98d70144956bc7385ec9293680eefa139f3f879
Bajram Shehi <bajram.shehi98@gmail.com>
2024-11-11 14:15:55 +0100

    Refactor code

--- ParcelPriceOptimizer.DAL/Entities/CustomerInput.cs
+++ ParcelPriceOptimizer.DAL/Entities/CustomerInput.cs
@@ -8,18 +8,14 @@ namespace ParcelPriceOptimizer.DAL.Entities
 {
     public class CustomerInput : BaseEntityWithKey
     {
-        public int ParcelId { get; set; }
-        public Parcel Parcel { get; set; }
         public string UserId { get; set; }
         public ApplicationUser User { get; set; }
-        public string CustomerName { get; set; }
-        public string CustomerEmail { get; set; }
-        public DateTime SubmittedAt { get; set; } = DateTime.UtcNow;
         public decimal Width { get; set; }
         public decimal Height { get; set; }
         public decimal Depth { get; set; }
         public decimal Volume => Width * Height * Depth;
         public decimal Weight { get; set; }
         public decimal Price { get; set; }
+        public DateTime SubmittedAt { get; set; } = DateTime.UtcNow;
     }
 }
