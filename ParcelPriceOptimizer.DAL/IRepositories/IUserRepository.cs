c3f93e9fb850e0e3a3a73f273410bff28feae4b3
Bajram Shehi <bajram.shehi98@gmail.com>
2024-11-11 14:22:37 +0100

    User Repository and Interface implementation

---
+++ ParcelPriceOptimizer.DAL/IRepositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using ParcelPriceOptimizer.DAL.Entities;
+
+namespace ParcelPriceOptimizer.DAL.IRepositories
+{
+    public interface IUserRepository
+    {
+        string GetCurrentUserId();
+        Task<ApplicationUser> GetByIdAsync(string userId);
+        Task<IEnumerable<ApplicationUser>> GetAllAsync();
+    }
+}
---
+++ ParcelPriceOptimizer.DAL/Repositories/UserRepository.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ParcelPriceOptimizer.DAL.Entities;
+using ParcelPriceOptimizer.DAL.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParcelPriceOptimizer.DAL.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _applicationDbContext;
+        private readonly ILogger<UserRepository> _logger;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserRepository(ApplicationDbContext applicationDbContext,
+                              UserManager<ApplicationUser> userManager,
+                              ILogger<UserRepository> logger,
+                              IHttpContextAccessor httpContextAccessor)
+        {
+            _applicationDbContext = applicationDbContext;
+            _userManager = userManager;
+            _logger = logger;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string GetCurrentUserId()
+        {
+            var claims = _httpContextAccessor.HttpContext.User.Claims;
+
+            foreach (var claim in claims)
+            {
+                Console.WriteLine($"Claim Type: {claim.Type}, Claim Value: {claim.Value}");
+            }
+
+            var userId = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (string.IsNullOrEmpty(userId))
+            {
+                throw new Exception("User ID not found in claims.");
+            }
+
+            Console.WriteLine($"Retrieved User ID: {userId}");
+            return userId;
+        }
+
+        public async Task<ApplicationUser> GetByIdAsync(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+
+            if (user == null)
+            {
+                _logger.LogWarning($"User not found with ID: {userId}");
+            }
+
+            return user;
+        }
+
+        public async Task<IEnumerable<ApplicationUser>> GetAllAsync()
+        {
+            return await _applicationDbContext.ApplicationUsers.ToListAsync();
+        }
+    }
+}
