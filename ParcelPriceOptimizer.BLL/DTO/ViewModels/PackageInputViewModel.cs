19b76408abd5d0f46b6d4496ff619ca549dd0702
Bajram Shehi <bajram.shehi98@gmail.com>
2024-11-11 14:33:20 +0100

    Refactor code

--- ParcelPriceOptimizer.BLL/DTO/ViewModels/PackageInputViewModel.cs
+++ ParcelPriceOptimizer.BLL/DTO/ViewModels/PackageInputViewModel.cs
@@ -1,24 +1,16 @@
-﻿using ParcelPriceOptimizer.DAL.Entities;
+﻿using ParcelPriceOptimizer.BLL.IServices;
+using ParcelPriceOptimizer.DAL.Entities;
 
 namespace ParcelPriceOptimizer.BLL.DTO.ViewModels
 {
     public class PackageInputViewModel
     {
-        //public int ParcelId { get; set; }
-        //public Parcel Parcel { get; set; }
-        //public string UserId { get; set; }
-        //public ApplicationUser User { get; set; }
-
-        public string CustomerName { get; set; }
-        public string CustomerEmail { get; set; }
-
+        public string UserId { get; set; }
         public decimal Width { get; set; }
         public decimal Height { get; set; }
         public decimal Depth { get; set; }
         public decimal Weight { get; set; }
-
         public DateTime SubmittedAt { get; set; } = DateTime.UtcNow;
-
         public decimal Volume => Width * Height * Depth;
     }
 }
