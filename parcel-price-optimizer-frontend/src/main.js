96de0ce029bb361138161f9d4c1326f74c1d2211
Bajram Shehi <bajram.shehi98@gmail.com>
2024-11-11 16:32:14 +0100

    Imported font-awesome and bootstrap icons libraries in main class of the project

--- parcel-price-optimizer-frontend/src/main.js
+++ parcel-price-optimizer-frontend/src/main.js
@@ -3,5 +3,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
 import store from './store';
+import '@fortawesome/fontawesome-free/css/all.css'; 
+import '@fortawesome/fontawesome-free/js/all.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 createApp(App).use(router).use(store).mount('#app')
