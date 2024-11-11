ebcbab064f32f290ec7cae6a48ede1f4d6df066e
Bajram Shehi <bajram.shehi98@gmail.com>
2024-11-11 14:27:16 +0100

    User Service and Interface implementation

---
+++ ParcelPriceOptimizer.BLL/IServices/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParcelPriceOptimizer.BLL.IServices
+{
+    public interface IUserService
+    {
+        string GetCurrentUserId();
+    }
+}
---
+++ ParcelPriceOptimizer.BLL/Services/UserService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using ParcelPriceOptimizer.BLL.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParcelPriceOptimizer.BLL.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string GetCurrentUserId()
+        {
+            var claims = _httpContextAccessor.HttpContext.User.Claims;
+
+            foreach (var claim in claims)
+            {
+                Console.WriteLine($"Claim Type: {claim.Type}, Claim Value: {claim.Value}");
+            }
+
+            var userId = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value
+                         ?? _httpContextAccessor.HttpContext.User.FindFirst("sub")?.Value;
+
+            if (string.IsNullOrEmpty(userId))
+            {
+                throw new Exception("User ID claim not found.");
+            }
+
+            return userId;
+        }
+    }
+}
