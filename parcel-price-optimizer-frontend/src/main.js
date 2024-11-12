d8f1ba4ad9fea4d11130151efdd74b765f64332e
Bajram Shehi <bajram.shehi98@gmail.com>
2024-11-12 17:23:18 +0100

    Imported App and createApp to main.js

--- parcel-price-optimizer-frontend/src/main.js
+++ parcel-price-optimizer-frontend/src/main.js
@@ -1,9 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 import router from './router';
 import store from './store';
-import '@fortawesome/fontawesome-free/css/all.css'; 
+import '@fortawesome/fontawesome-free/css/all.css';
 import '@fortawesome/fontawesome-free/js/all.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
