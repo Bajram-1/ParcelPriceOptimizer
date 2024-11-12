ff7518183f8128c24d7e1c6b828c0e6e50152c1e
Bajram Shehi <bajram.shehi98@gmail.com>
2024-11-12 17:10:08 +0100

    Created Parcel Payment View Model

---
+++ ParcelPriceOptimizer.BLL/DTO/ViewModels/ParcelPaymentViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParcelPriceOptimizer.BLL.DTO.ViewModels
+{
+    public class ParcelPaymentViewModel
+    {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public int Width { get; set; } 
+        public int Height { get; set; } 
+        public int Depth { get; set; } 
+        public int Weight { get; set; }
+        public string UserId { get; set; }
+    }
+}
