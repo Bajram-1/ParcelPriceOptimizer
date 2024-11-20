03bc50d496a73a04b97d4668ad22da9decc1764c
Bajram Shehi <bajram.shehi98@gmail.com>
2024-11-20 18:56:58 +0100

    Added Courier Name and Volume properties in ParcelPaymentViewModel

--- ParcelPriceOptimizer.BLL/DTO/ViewModels/ParcelPaymentViewModel.cs
+++ ParcelPriceOptimizer.BLL/DTO/ViewModels/ParcelPaymentViewModel.cs
@@ -9,10 +9,12 @@ namespace ParcelPriceOptimizer.BLL.DTO.ViewModels
     public class ParcelPaymentViewModel
     {
         public decimal Price { get; set; }
-        public int Width { get; set; } 
-        public int Height { get; set; } 
-        public int Depth { get; set; } 
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int Depth { get; set; }
         public int Weight { get; set; }
+        public decimal Volume => Width * Height * Depth;
         public string? UserId { get; set; }
+        public string Courier { get; set; }
     }
 }
