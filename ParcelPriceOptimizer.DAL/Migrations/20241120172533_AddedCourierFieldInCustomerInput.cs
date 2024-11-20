e6e57c2bd55c2f7a924e2d4e77b9839bac6d7727
Bajram Shehi <bajram.shehi98@gmail.com>
2024-11-20 18:47:14 +0100

    Migrations

---
+++ ParcelPriceOptimizer.DAL/Migrations/20241120172533_AddedCourierFieldInCustomerInput.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ParcelPriceOptimizer.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCourierFieldInCustomerInput : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Courier",
+                table: "CustomerInputs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Courier",
+                table: "CustomerInputs");
+        }
+    }
+}
