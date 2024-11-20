bf31d0115dae77f484df1ac8a550f33a60f9c547
Bajram Shehi <bajram.shehi98@gmail.com>
2024-11-20 12:15:36 +0100

    Migrations

---
+++ ParcelPriceOptimizer.DAL/Migrations/20241120111451_RemovedShippingQuoteFromDb.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ParcelPriceOptimizer.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedShippingQuoteFromDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ShippingQuotes");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ShippingQuotes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CourierId = table.Column<int>(type: "int", nullable: false),
+                    CalculatedPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    QuotedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShippingQuotes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ShippingQuotes_Couriers_CourierId",
+                        column: x => x.CourierId,
+                        principalTable: "Couriers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShippingQuotes_CourierId",
+                table: "ShippingQuotes",
+                column: "CourierId");
+        }
+    }
+}
